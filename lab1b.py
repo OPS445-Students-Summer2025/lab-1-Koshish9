68aad6696540dc6199c70056d4d2ae7c16b51c8c
Koshish Adhikari <kadhikari9@myseneca.ca>
2025-05-26 16:33:34 -0400

    Individual message or note.

---
+++ lab1b.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+'''Ask about Isaac's age'''
+name = 'Isaac'
+print('How old are you ' + name + '?')
---
+++ lab1d.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+'''PEMDAS example'''
+x = 10
+y = 2
+z = 5
+print(str(x) + " + " + str(y) + " * " + str(z) + " = " + str(x + y * z))
